aca0a8149b3142dd66805a80c99b39269e5b6790
vusal5555 <vusal.novruzov.1@iliauni.edu.ge>
2025-12-19 12:57:38 +0400

    fixed cors issues 2

--- app.py
+++ app.py
@@ -68,6 +68,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "https://ai-blogpost-generator-frontend.vercel.app/",
+        "https://*.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
