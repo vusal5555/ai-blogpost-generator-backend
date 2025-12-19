fa46eb5683fbeae637c9d10bb8ee7c6e9c6266b7
vusal5555 <vusal.novruzov.1@iliauni.edu.ge>
2025-12-19 12:53:59 +0400

    fixed cors issues

--- app.py
+++ app.py
@@ -65,7 +65,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://ai-blogpost-generator-frontend.vercel.app/",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
